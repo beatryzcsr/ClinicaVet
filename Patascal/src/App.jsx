65a6b461fe3cb4be789f5386c87076dba854323b
marcela <marceladacruz779@gmail.com>
2026-08-18 10:20:31 -0300

    marcela2

--- Patascal/src/App.jsx
+++ Patascal/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Products from './pages/Produtos';
 import FAQ from './pages/FAQ';
+import Contato from './pages/Contato';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/produtos" element={<Products />} />
         <Route path="/faq" element={<FAQ />} />
+        <Route path="/contato" element={<Contato />} />
       </Routes>
     </BrowserRouter>
   );
