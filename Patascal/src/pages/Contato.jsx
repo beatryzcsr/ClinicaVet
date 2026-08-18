bfd1bc48db2bcc75d6c91d127a92b1db02806dec
marcela <marceladacruz779@gmail.com>
2026-08-18 11:11:52 -0300

    MARCELA4

--- Patascal/src/pages/Contato.jsx
+++ Patascal/src/pages/Contato.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import furao from '../../public/furão1.png'
 
 export default function Contato() {
   const [form, setForm] = useState({
@@ -31,70 +32,65 @@ export default function Contato() {
   return (
     <>
       <section
-        className="flex min-h-screen items-end bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url('/img/contatos.jpg')" }}
+        className="flex bg-cover bg-center fix  "
+        style={{ backgroundImage: `url(${furao})` }}
       >
-        <h1 className="ml-2 text-5xl text-[#e6d8a8] md:text-6xl">Contatos</h1>
-      </section>
-
-      <section className="min-h-[600px] bg-[#e6d8a8] px-5 py-10 md:px-20">
+      <section className="min-h-[600px] px-5 py-10 md:px-20 p-10 mt-10">
         <div className="space-y-4 text-sm leading-8 text-[#333]">
-          <p>patascal@gmail.com</p>
-          <p>(19) 99999-9999</p>
-          <p>Assunto</p>
-          <span>Nos sua mensagem por aqui</span>
         </div>
 
-        <div className="mx-auto my-14 w-full max-w-[420px] rounded-xl bg-[#efe5c6] p-8 shadow-sm">
+        <div className=" my-17 rounded-xl bg-emerald-100 p-15 shadow-sm">
           <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-            <label className="text-xs">Nome</label>
+            <label className="text-left text-xs w-20">Nome</label>
             <input
               type="text"
               name="nome"
               value={form.nome}
               onChange={handleChange}
               required
-              className="h-7 rounded-md border border-[#ff7a7a] bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-[#efb1b1]"
+              className="h-7 rounded-md border bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-emerald-100"
             />
 
-            <label className="text-xs">Email</label>
+            <label className="text-left text-xs">Email</label>
             <input
               type="email"
               name="email"
               value={form.email}
               onChange={handleChange}
               required
-              className="h-7 rounded-md border border-[#ff7a7a] bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-[#efb1b1]"
+              className="h-7 rounded-md border bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-emerald-100"
             />
-              <label className="text-xs">Assunto</label>
+            <label className="text-left text-xs">Assunto</label>
             <input
               type="text"
               name="assunto"
               value={form.assunto}
               onChange={handleChange}
               required
-              className="h-7 rounded-md border border-[#ff7a7a] bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-[#efb1b1]"
+              className="h-7 rounded-md border bg-white px-2 py-1 focus:outline-none focus:ring-2 focus:ring-emerald-100"
             />
 
-            <label className="text-xs">Mensagem</label>
+            <label className="text-left text-xs">Mensagem</label>
             <textarea
               name="mensagem"
               value={form.mensagem}
               onChange={handleChange}
               rows="6"
               required
-              className="resize-none rounded-lg border border-[#ff7a7a] bg-white p-2 focus:outline-none focus:ring-2 focus:ring-[#efb1b1]"
+              className="resize-none rounded-lg border bg-white p-2 focus:outline-none focus:ring-2 focus:ring-emerald-100"
             ></textarea>
 
             <button
               type="submit"
-              className="mt-2 cursor-pointer rounded-md border-0 bg-[#efb1b1] px-4 py-2 text-xs text-white transition duration-300 hover:bg-[#e89a9a]"
+              className="mt-2 cursor-pointer rounded-md border-0 bg-emerald-300 px-4 py-2 text-xs text-white transition duration-300 hover:bg-emerald-500"
             >
               Enviar
             </button>
           </form>
         </div>
       </section>
+      </section>
+
     </>
   )
 }
