3c1a05c60a26b23bf609dee53421f3d55e73704b
BeatrizOliveira15 <beatriz.s.oliveira15@aluno.senai.br>
2026-08-18 10:01:56 -0300

    array e rota do FAQ

---
+++ Patascal/src/products/FAQCard.jsx
@@ -0,0 +1,41 @@
+import tucano from "../../public/tucano.png";
+import camaleao from "../../public/camaleao.png";
+import capivara from "../../public/capivara.png";
+import chinchila from "../../public/chinchila.png";
+import peixe from "../../public/peixe.png";
+import araras from  "../../public/araras.png";
+ 
+export const faq = [
+    {
+        question: 'Quais animais vocês atendem?',
+        answer: 'Atendemos animais exóticos e silvestres, como aves, répteis, peixes, roedores e pequenos mamíferos.',
+        image: tucano,
+    },
+    {
+        question: 'Preciso agendar uma consulta?',
+        answer: 'Sim! O agendamento ajuda a garantir um atendimento adequado para cada animal. Basta acessar a página de contato!',
+        image: camaleao,
+    },
+    {
+        question: 'Vocês atendem animais silvestres?',
+        answer: 'Sim, realizamos atendimento veterinário para animais silvestres dentro das normas legais.',
+        image: capivara,
+    },
+    {
+        question: 'O que devo levar para a consulta?',
+        answer: 'Leve o animal e, se possível, informações sobre sua alimentação, rotina e histórico de saúde.',
+        image: chinchila,
+    },
+    {
+        question: 'Vocês realizam exames?',
+        answer: 'Sim, pois dependendo do caso, podem ser solicitados exames para auxiliar no diagnóstico.',
+        image: peixe,
+    },
+    {
+        question: 'Como saber se meu animal está doente?',
+        answer: 'Mudanças no comportamento, alimentação, peso ou aparência podem indicar algum problema. Em caso de dúvida, nos procure!',
+        image: araras,
+    }
+];
+ 
+export default faq;
