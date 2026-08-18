fb2bf92e20920d1abbfc6c20b836097a13722ce3
Isadora-Danelon <isadora.danelon@aluno.senai.br>
2026-08-18 11:55:14 -0300

    Estilização final

--- Patascal/src/pages/Sobre.jsx
+++ Patascal/src/pages/Sobre.jsx
@@ -8,17 +8,16 @@ export default function Sobre() {
     return (
         <div
             id="sobre"
-            className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${fundoAgua})` }}
+            className="min-h-screen w-full bg-cover bg-center bg-no-repeat bg-[#D0FAE5]"
+            
         >
-                    <div className="absolute inset-0 bg-black/40 " />
             <div className="min-h-screen w-full bg-white/5 backdrop-blur-[1px]">
                 <div className="relative mx-auto flex w-full max-w-[1500px] flex-col items-center justify-center px-4 pb-3 pt-8">
                     <div className="mb-6 mt-23 flex items-center justify-center">
                         <img
                             src={logoMedVet}
                             alt="Logo"
-                            className="h-28 w-28 rounded-full border-[6px] border-[#d4af37] bg-[#f9f9f9] object-cover shadow-[0_0_0_3px_rgba(20,83,45,0.18)]"
+                            className="h-28 w-28 rounded-full border-[6px] border-[#d4af37] bg-[#B6DBD9] object-cover shadow-[0_0_0_3px_rgba(20,83,45,0.18)]"
                         />
                     </div>
 
@@ -33,7 +32,7 @@ export default function Sobre() {
                             </div>
                         </div>
 
-                        <div className="flex h-[340px] w-[550px] items-center justify-center rounded-[26px] border-[3px] border-[#d9d9d9] bg-[#f5f5f5] shadow-[inset_0_0_0_1px_rgba(0,0,0,0.02)]">
+                        <div className="flex h-[340px] w-[550px] items-center justify-center rounded-[26px] border-[3px] border-[#d9d9d9] bg-[#00BC7D] shadow-[inset_0_0_0_1px_rgba(0,0,0,0.02)]">
                            
                             <p className="flex text-justify p-10 mt-3 mb-3"> A Patascal é uma clínica veterinária especializada no cuidado de animais exóticos e silvestres, oferecendo atendimento dedicado, seguro e humanizado para cada espécie. Nossa equipe busca proporcionar o melhor cuidado para animais que precisam de atenção especializada, respeitando suas características, necessidades e bem-estar. Na Patascal, cada paciente é tratado com carinho, responsabilidade e conhecimento, porque toda vida merece cuidado.</p>
 
